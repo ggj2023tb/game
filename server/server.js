0d58d78da8d711c844dce384a512e668f90aecf0
Dominik Meyer <domeyer@TB-L169.corp.ad.zalando.net>
2023-02-03 21:58:48 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 9000 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function (msg) {
+        obj = JSON.parse(msg);
+
+        if (obj.type == 'startGame') {
+            for (let i=0; i<4; i++) {
+                ws.send(JSON.stringify({ "type": "addPlayer" }));
+            }
+        }
+
+        if (obj.type == 'sendQuestion') {
+            ws.send(JSON.stringify({ "roomId": "123", "type": "question", "msg": "what is 1+1", "options": ["3", "4", "5", "2"] }));
+        }
+    });
+});
+
